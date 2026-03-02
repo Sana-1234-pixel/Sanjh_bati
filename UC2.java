c96b1b117c2aef70af4089ed2b39da0d845a27f5
Sanjhbati Mukherjee <your_github_email@example.com>
2026-03-02 09:26:17 +0530

    Added UC2.java

---
+++ UC2.java
@@ -0,0 +1,15 @@
+public class UC2 {
+    public static void main(String[] args) {
+        // Each line is 9 characters wide per letter
+        // Concatenate strings to form the banner row by row
+
+        System.out.println("  *****   " + "  *****   " + "  *****   " + " ******* ");
+        System.out.println(" *     *  " + " *     *  " + " *     *  " + " *       ");
+        System.out.println("*       * " + "*       * " + "*       * " + " *       ");
+        System.out.println("*       * " + "*       * " + "*      * " + "  ******* ");
+        System.out.println("*       * " + "*       * " + "* *****    " + "      * ");
+        System.out.println(" *     *  " + " *     *  " + "*         " + "       * ");
+        System.out.println("  *****   " + "  *****   " + "*         " + " *******  ");
+    }
+}
+
