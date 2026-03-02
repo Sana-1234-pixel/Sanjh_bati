9e21ea5b1672c8c0a6ce6f6a1a4552535a260a1c
Sanjhbati Mukherjee <your_github_email@example.com>
2026-03-02 10:50:43 +0530

    Added UC1.java

--- UC1.java
+++ UC1.java
@@ -1,4 +1,4 @@
-public class OOPSBannerApp {
+public class UC1 {
     public static void main(String[] args) {
         // Goal: Display the literal text "OOPS" once on the console [cite: 15]
         System.out.println("OOPS");
